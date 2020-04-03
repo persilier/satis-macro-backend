3429f6efa6fc1157b639fe18d63b27c437eb34a3
Christian AWASSI <christian@dmdconsult.com>
2020-04-03 09:43:15 +0100

    Resolve conflit metadata branch develop

--- modules/metadata-package/config/metadata.php
+++ modules/metadata-package/config/metadata.php
@@ -1,22 +1,45 @@
+
 <?php
-
-
 return [
+    'type' => [
+        'models', 'forms','action-forms'
+    ],
 
     'models' => [
         'isValid' => 'name',
         'rules'   => [
-                'name' => 'required|string',
-                'description' => 'required|string',
-                'fonction' => 'required|string',
-            ]
+            'name' => 'required|string|max:50',
+            'description' => 'required|string|max:255',
+            'fonction' => 'required|string',
+        ],
+        'fillable' => [
+            'name', 'description', 'fonction'
+        ]
     ],
 
     'forms' => [
         'isValid' => 'name',
         'rules'   => [
+            'name' => 'required|string|max:50',
+            'description' => 'required|string|max:255',
+            'content_default' => 'required|array',
+        ],
+        'fillable' => [
+            'name', 'description','content_default'
+        ],
+        'isNotDelete' => 'content'
+    ],
 
-
+    'action-forms' => [
+        'isValid' => 'name',
+        'rules'   => [
+            'name' => 'required|string|max:50',
+            'description' => 'required|string|max:255',
+            'endpoint' => 'required|string',
+        ],
+        'fillable' => [
+            'name', 'description', 'endpoint'
         ]
     ]
+
 ];
