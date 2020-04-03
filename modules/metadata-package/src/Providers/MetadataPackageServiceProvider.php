529c3a0bcb170249362fd0b7a463daa71eb42ad1
Christian AWASSI <christian@dmdconsult.com>
2020-04-03 09:57:11 +0100

    Resolve conflit provider metadata

--- modules/metadata-package/src/Providers/MetadataPackageServiceProvider.php
+++ modules/metadata-package/src/Providers/MetadataPackageServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace Satis2020\MetadataPackage\Providers;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Satis2020\ServicePackage\Models\Metadata;
@@ -29,7 +30,6 @@ class MetadataPackageServiceProvider extends ServiceProvider
         Route::bind('metadata', function($value){
             return Metadata::where('name',$value)->get()->first() ?? abort(404);
         });
-        
     }
 
     /**
@@ -53,9 +53,9 @@ class MetadataPackageServiceProvider extends ServiceProvider
 
 
     protected function registerConfig(){
-        
+
         //if (! $this->app->configurationIsCached()) {
-            $this->mergeConfigFrom(__DIR__.'/../../config/metadata.php','metadata');
+        $this->mergeConfigFrom(__DIR__.'/../../config/metadata.php','metadata');
         //}
     }
 
