2b0a2a4640220b287a87b0c274e8a22731d3fd1c
Christian AWASSI <christianawassi@gmail.com>
2020-04-22 12:53:49 +0100

    Gestion des types et catégories clients

---
+++ modules/services-package/src/Models/CategoryClient.php
@@ -0,0 +1,47 @@
+<?php
+namespace Satis2020\ServicePackage\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Satis2020\ServicePackage\Traits\SecureDelete;
+use Satis2020\ServicePackage\Traits\UuidAsId;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class CategoryClient extends Model
+{
+
+    use UuidAsId, SoftDeletes, SecureDelete;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public $fillable = [
+        'name',
+        'description',
+        'institutions_id'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     **/
+    public function institution()
+    {
+        return $this->belongsTo(Institution::class, 'institutions_id', 'id');
+    }
+
+
+}
