66ce7c6fe6bbf8229bf5437af5b53d38b4e8de99
Christian AWASSI <christianawassi@gmail.com>
2020-06-01 17:51:49 +0100

    Correction de la route des unités dans institution

--- modules/institution/routes/api.php
+++ modules/institution/routes/api.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Route;
  */
 Route::resource('institutions', 'Institutions\InstitutionController')->except(['create', 'edit']);
 Route::name('institutions.update.logo')->post('institutions/{institution}/update-logo', 'Institutions\InstitutionController@updateLogo');
-Route::resource('institutions.units', 'Institutions\InstitutionUnitController', ['only' => ['index']]);
+Route::resource('institutions.units', 'Institutions\InstitutionUnitController')->only(['index']);
 
 /*
