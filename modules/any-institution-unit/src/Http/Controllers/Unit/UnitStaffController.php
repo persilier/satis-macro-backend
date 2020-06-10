cabcfb18ed593ea49c2d01d9a9f1ef235b2fe48b
christian <christian@dmdconsult.com>
2020-06-10 12:57:00 +0100

    Seeder Channels

--- modules/any-institution-unit/src/Http/Controllers/Unit/UnitStaffController.php
+++ modules/any-institution-unit/src/Http/Controllers/Unit/UnitStaffController.php
@@ -3,15 +3,15 @@
 namespace Satis2020\AnyInstitutionUnit\Http\Controllers\unit;
 
 use Satis2020\ServicePackage\Http\Controllers\ApiController;
-
+use Satis2020\ServicePackage\Models\Staff;
 class UnitStaffController extends ApiController
 {
 
     public function __construct()
     {
         parent::__construct();
-        //$this->middleware('auth:api');
-        //$this->middleware('permission:update-any-unit')->only(['index']);
+        $this->middleware('auth:api');
+        $this->middleware('permission:update-any-unit')->only(['index']);
 
     }
 
