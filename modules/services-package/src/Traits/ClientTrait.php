caa67047c9a97bd0a4f74ed17212bccc52f3c29a
christian <christian@dmdconsult.com>
2020-06-10 12:04:03 +0100

    Exception Traits client

--- modules/services-package/src/Traits/ClientTrait.php
+++ modules/services-package/src/Traits/ClientTrait.php
@@ -63,7 +63,7 @@ trait ClientTrait
 
     protected function handleAccountClient($number, $id)
     {
-        if(Account::where('client_institution_id', $id)->where('number', $number)->get())
+        if(Account::where('client_institution_id', $id)->where('number', $number)->first())
             return ['code' => 409,'status' => true, 'message' => 'Impossible d\'enregistrer deux fois le même numéro de compte pour une institution donnée.'];
         return ['status' => false];
     }
