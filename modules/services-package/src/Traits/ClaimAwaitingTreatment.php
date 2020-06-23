c467c158658a2411a825c0c58897f990a806d33e
christian <christian@dmdconsult.com>
2020-06-23 10:44:24 +0100

    Ajout de claim awaiting treatment au dépot de package

--- modules/services-package/src/Traits/ClaimAwaitingTreatment.php
+++ modules/services-package/src/Traits/ClaimAwaitingTreatment.php
@@ -6,10 +6,19 @@ use Satis2020\ServicePackage\Models\Claim;
 
 trait ClaimAwaitingTreatment
 {
-    protected function getAllClaimAwaitingTreatment(){
+    protected function getAllClaimAwaitingTreatment($staffId, $institutionId){
         try {
             $claims = Claim::with($this->getRelations())
-                ->where('institution_targeted_id',$institutionId)
+                ->where(function ($query) use ($institutionId){
+                    $query->whereHas('createdBy', function ($q) use ($institutionId){
+                        $q->where('institution_id', $institutionId);
+                    })
+                    ->orWhereHas('treatments', function ($q) use ($institutionId){
+                        $q->where('institution_id', $institutionId);
+                    });
+
+                })->orWhere('institution_targeted_id',$institutionId)
+
                 ->where('status', 'full')->get();
         } catch (\Exception $exception) {
             throw new CustomException("Impossible de récupérer les listes des réclamations");
