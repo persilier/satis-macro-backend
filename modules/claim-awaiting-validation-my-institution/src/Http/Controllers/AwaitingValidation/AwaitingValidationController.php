65a61a55dd3926f6bf6aaed763e32de304ae89de
Abdul AZONHOU <cmdtazonhou@gmail.com>
2020-06-29 15:58:47 +0100

    claim awaiting validation

---
+++ modules/claim-awaiting-validation-my-institution/src/Http/Controllers/AwaitingValidation/AwaitingValidationController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Satis2020\ClaimAwaitingValidationMyInstitution\Http\Controllers\AwaitingValidation;
+
+use Illuminate\Validation\ValidationException;
+use Satis2020\ServicePackage\Exceptions\CustomException;
+use Satis2020\ServicePackage\Http\Controllers\ApiController;
+use Satis2020\ServicePackage\Models\Claim;
+use Illuminate\Http\Request;
+use Satis2020\ServicePackage\Rules\TreatmentCanBeValidateRules;
+use Satis2020\ServicePackage\Traits\AwaitingValidation;
+
+class AwaitingValidationController extends ApiController
+{
+
+    use AwaitingValidation;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->middleware('auth:api');
+
+        $this->middleware('permission:list-claim-awaiting-validation-my-institution')->only(['index']);
+        $this->middleware('permission:show-claim-awaiting-validation-my-institution')->only(['show']);
+        $this->middleware('permission:validate-treatment-my-institution')->only(['validate', 'invalidate']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     * @throws \Satis2020\ServicePackage\Exceptions\RetrieveDataUserNatureException
+     */
+    public function index()
+    {
+        return response()->json($this->getClaimsAwaitingValidationInMyInstitution(), 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Claim $claim
+     * @return \Illuminate\Http\Response
+     * @throws CustomException
+     */
+    public function show(Claim $claim)
+    {
+        $claims = $this->getClaimsAwaitingValidationInMyInstitution();
+
+        if ($claims->search(function ($item, $key) use ($claim) {
+                return $item->id == $claim->id;
+            }) === false) {
+            Throw new CustomException('The claim can not be showed by this pilot', 409);
+        }
+
+        return response()->json($this->showClaim($claim), 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Claim $claim
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws ValidationException
+     * @throws \Satis2020\ServicePackage\Exceptions\RetrieveDataUserNatureException
+     */
+    public function validated(Request $request, Claim $claim)
+    {
+
+        $claim->load($this->getRelations());
+
+        $request->merge(['claim' => $claim->id]);
+
+        $rules = [
+            'claim' => new TreatmentCanBeValidateRules($this->institution()->id),
+            'solution_communicated' => 'required|string'
+        ];
+
+        $this->validate($request, $rules);
+
+        return response()->json($this->handleValidate($request, $claim), 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Claim $claim
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws ValidationException
+     * @throws \Satis2020\ServicePackage\Exceptions\RetrieveDataUserNatureException
+     */
+    public function invalidated(Request $request, Claim $claim)
+    {
+
+        $claim->load($this->getRelations());
+
+        $request->merge(['claim' => $claim->id]);
+
+        $rules = [
+            'claim' => new TreatmentCanBeValidateRules($this->institution()->id),
+            'invalidated_reason' => 'required|string'
+        ];
+
+        $this->validate($request, $rules);
+
+        return response()->json($this->handleInvalidate($request, $claim), 201);
+    }
+}
---
+++ modules/services-package/src/Traits/AwaitingValidation.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace Satis2020\ServicePackage\Traits;
+
+
+use Carbon\Carbon;
+use Satis2020\ServicePackage\Models\Claim;
+
+trait AwaitingValidation
+{
+    protected function getClaimsAwaitingValidationInMyInstitution($institution_id = null)
+    {
+        $institution_id = is_null($institution_id)
+            ? $this->institution()->id
+            : $institution_id;
+
+        $claimsTreated = Claim::with($this->getRelations())->where('status', 'treated')->get();
+        return $claimsTreated->filter(function ($value, $key) use ($institution_id) {
+            $value->activeTreatment->load($this->getActiveTreatmentRelations());
+            return $value->activeTreatment->responsibleStaff->institution_id == $institution_id;
+        });
+    }
+
+    protected function getRelations()
+    {
+        return [
+            'claimObject.claimCategory', 'claimer', 'relationship', 'accountTargeted', 'institutionTargeted', 'unitTargeted', 'requestChannel',
+            'responseChannel', 'amountCurrency', 'createdBy.identite', 'completedBy.identite', 'files', 'activeTreatment'
+        ];
+    }
+
+    protected function getActiveTreatmentRelations()
+    {
+        return [
+            'responsibleUnit', 'assignedToStaffBy.identite', 'responsibleStaff.identite'
+        ];
+    }
+
+    protected function handleValidate($request, $claim)
+    {
+        $claim->activeTreatment()->update(['solution_communicated' => $request->solution_communicated, 'validated_at' => Carbon::now()]);
+
+        $claim->update(['status' => 'validated']);
+
+        return $claim;
+    }
+
+    protected function handleInvalidate($request, $claim)
+    {
+        $claim->activeTreatment()->update(['invalidated_reason' => $request->invalidated_reason, 'validated_at' => Carbon::now()]);
+
+        $claim->update(['status' => 'assigned_to_staff']);
+
+        return $claim;
+    }
+
+    protected function showClaim($claim)
+    {
+        $claim->load($this->getRelations());
+        $claim->activeTreatment->load($this->getActiveTreatmentRelations());
+        return $claim;
+    }
+}
