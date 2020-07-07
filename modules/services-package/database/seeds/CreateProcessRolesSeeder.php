bd9c6a98bb593d1a377b9e5ba5e0a4122778c9b1
Abdul AZONHOU <ulrich@dmdconsult.com>
2020-07-07 18:01:48 +0100

    Fix CreateProcessRolesSeeder

--- modules/services-package/database/seeds/CreateProcessRolesSeeder.php
+++ modules/services-package/database/seeds/CreateProcessRolesSeeder.php
@@ -34,28 +34,86 @@ class CreateProcessRolesSeeder extends Seeder
     public function run()
     {
 
-        $collection = collect([
-            'collector-holding' => [
-                'store-claim-against-any-institution'
-            ],
-            'collector-filial-pro' => [
-                'store-claim-against-my-institution'
-            ],
-            'collector-observatory' => [
-                'store-claim-without-client'
-            ],
-            'staff' => [
-                'list-claim-awaiting-treatment',
-                'show-claim-awaiting-treatment',
-                'rejected-claim-awaiting-treatment',
-                'self-assignment-claim-awaiting-treatment',
-                'assignment-claim-awaiting-treatment',
-                'list-claim-assignment-to-staff',
-                'show-claim-assignment-to-staff',
-            ]
-        ]);
+        $nature = env('APP_NATURE');
 
-        $collection->map(function ($item, $key) {
+        if ($nature === 'DEVELOP') {
+            $collection = collect([
+                'collector-holding' => [
+                    'store-claim-against-any-institution'
+                ],
+                'collector-filial-pro' => [
+                    'store-claim-against-my-institution'
+                ],
+                'collector-observatory' => [
+                    'store-claim-without-client'
+                ],
+                'staff' => [
+                    'list-claim-awaiting-treatment',
+                    'show-claim-awaiting-treatment',
+                    'rejected-claim-awaiting-treatment',
+                    'self-assignment-claim-awaiting-treatment',
+                    'assignment-claim-awaiting-treatment',
+                    'list-claim-assignment-to-staff',
+                    'show-claim-assignment-to-staff',
+                ]
+            ]);
+        }
+
+        if ($nature === 'MACRO') {
+            $collection = collect([
+                'collector-holding' => [
+                    'store-claim-against-any-institution'
+                ],
+                'collector-filial-pro' => [
+                    'store-claim-against-my-institution'
+                ],
+                'staff' => [
+                    'list-claim-awaiting-treatment',
+                    'show-claim-awaiting-treatment',
+                    'rejected-claim-awaiting-treatment',
+                    'self-assignment-claim-awaiting-treatment',
+                    'assignment-claim-awaiting-treatment',
+                    'list-claim-assignment-to-staff',
+                    'show-claim-assignment-to-staff',
+                ]
+            ]);
+        }
+
+        if ($nature === 'HUB') {
+            $collection = collect([
+                'collector-observatory' => [
+                    'store-claim-without-client'
+                ],
+                'staff' => [
+                    'list-claim-awaiting-treatment',
+                    'show-claim-awaiting-treatment',
+                    'rejected-claim-awaiting-treatment',
+                    'self-assignment-claim-awaiting-treatment',
+                    'assignment-claim-awaiting-treatment',
+                    'list-claim-assignment-to-staff',
+                    'show-claim-assignment-to-staff',
+                ]
+            ]);
+        }
+
+        if ($nature === 'PRO') {
+            $collection = collect([
+                'collector-filial-pro' => [
+                    'store-claim-against-my-institution'
+                ],
+                'staff' => [
+                    'list-claim-awaiting-treatment',
+                    'show-claim-awaiting-treatment',
+                    'rejected-claim-awaiting-treatment',
+                    'self-assignment-claim-awaiting-treatment',
+                    'assignment-claim-awaiting-treatment',
+                    'list-claim-assignment-to-staff',
+                    'show-claim-assignment-to-staff',
+                ]
+            ]);
+        }
+
+        $collection->map(function ($item, $key) use ($nature) {
             $this->createRole($key, $item);
             return $item;
         });
