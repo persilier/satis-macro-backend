b315f4557bd536ddf65cf7c5671c0690ff0eed56
Christian AWASSI <christianawassi@gmail.com>
2020-08-11 17:05:07 +0100

    création de package any user

---
+++ modules/any-user/route/api.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('/any')->name('any.')->group(function () {
+
+
+
+});
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+
+/*
+ * Roles
+ */
+/*
+Route::resource('roles', 'Role\RoleController', ['except' => ['create', 'edit']]);
+Route::resource('roles.permissions', 'Role\RolePermissionController', ['only' => ['store', 'destroy']]);
+Route::name('give.all.permissions')->post('give-all-permissions', 'Role\RolePermissionController@give_all');
+
+/*
+ * Permissions, Identites, Staffs, Clients
+ */
+/*
+Route::apiResource('permissions', 'Permission\PermissionController');
+Route::apiResource('identites', 'Identite\IdentiteController');
+Route::resource('identites.client-from-my-institution', 'Identite\IdentiteClientController', ['only' => ['store']]);
+/**
+ * Users
+ */
+/*
+Route::resource('users', 'User\UserController', ['except' => ['edit', 'update']]);
+Route::resource('users.roles', 'User\UserRoleController', ['only' => ['index', 'store']]);
+Route::resource('users.permissions', 'User\UserPermissionController', ['only' => ['index']]);
+Route::name('verify')->get('users/verify/{token}', 'User\UserController@verify');
+Route::name('resend')->get('users/{user}/resend', 'User\UserController@resend');
+
