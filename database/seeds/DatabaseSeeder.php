104b0b04a20fae8774f596fff5092d03be668039
Christian AWASSI <christianawassi@gmail.com>
2020-11-26 08:46:59 +0100

    Seeder module

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -125,6 +125,7 @@ class DatabaseSeeder extends Seeder
 //        $this->call(\Satis2020\ServicePackage\Database\Seeds\RecurrenceReclamationSeeder::class);
 //        $this->call(\Satis2020\Notification\Database\Seeds\RecurrenceNotificationsTableSeeder::class);
 //        $this->call(\Satis2020\ServicePackage\Database\Seeds\RejectUnitTransferLimitationSeeder::class);
+        $this->call(\Satis2020\ServicePackage\Database\Seeds\ModulePermissionsSeeder::class);
 
     }
 
