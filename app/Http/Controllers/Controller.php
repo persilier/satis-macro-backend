2ba645e93aa45aa3935e870c28ba413a5aab7568
Nelson <ulrich@dmdconsult.com>
2021-02-16 10:14:30 +0100

    test revive staff notification

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,19 +16,29 @@ use Satis2020\ServicePackage\Models\Message;
 use Satis2020\ServicePackage\Models\Staff;
 use Satis2020\ServicePackage\Traits\CreateClaim;
 use Satis2020\ServicePackage\Traits\DataUserNature;
-use Satis2020\ServicePackage\Traits\Notification;
+use Satis2020\ServicePackage\Traits\Notification as NotificationTrait;
 use Satis2020\ServicePackage\Models\Claim;
 use Satis2020\ServicePackage\Notifications\Recurrence;
+use Illuminate\Support\Facades\Notification;
+use Satis2020\ServicePackage\Notifications\ReviveStaff;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Notification, CreateClaim, DataUserNature;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, NotificationTrait, CreateClaim, DataUserNature;
 
     public function index()
     {
-        $claim = Claim::findOrFail("e74c2f16-a603-4147-8489-78d485a23a11");
-        dd(Claim::all());
-        return response()->json($this->getStaffToReviveIdentities($claim), 200);
+        $claim = Claim::findOrFail("4a6f1d6d-85d6-4349-a28b-32116dde7806");
+
+        $identities = $this->getStaffToReviveIdentities($claim);
+
+        //$identities[1]->notify(new ReviveStaff($claim, "CC"));
+        
+        Notification::send($this->getStaffToReviveIdentities($claim), new ReviveStaff($claim, "CC"));
+
+        dd($identities[1]);
+        
+        return response()->json([], 200);
     }
 
     public function download(File $file)
