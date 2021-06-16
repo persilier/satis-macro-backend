5c69552f82c0e9729bcbf0c6dd825979c190d388
Nelson <ulrich@dmdconsult.com>
2021-06-16 18:31:44 +0100

    redefine tracked files

--- modules/search-claim-any-reference/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('/any')->name('any.')->group(function () {
-
-    Route::get('search-claim/{reference}', 'Search\SearchController@index')->name('search.claim');
-});
--- modules/services-package/resources/views/mail/claim/feedback.blade.php
+++
@@ -1,6 +0,0 @@
-@component('mail::message')
-# Salut {{ $name }},
-
-{{ $text }}
-
-@endcomponent
