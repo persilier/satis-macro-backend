8f0a08a8c542487801ec6a5b3f1782fa0f15c707
Arafath ATTA YAYA <arafath@dmdconsult.com>
2022-01-18 17:26:03 +0100

    block user creating another active session

--- modules/services-package/src/Listeners/RevokeExistingTokens.php
+++ modules/services-package/src/Listeners/RevokeExistingTokens.php
@@ -2,9 +2,7 @@
 
 namespace Satis2020\ServicePackage\Listeners;
 
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Facades\Log;
+use Satis2020\ServicePackage\Exceptions\TwoSessionNotAllowed;
 use Satis2020\ServicePackage\Models\User;
 
 class RevokeExistingTokens
@@ -27,9 +25,11 @@ class RevokeExistingTokens
      */
     public function handle($event)
     {
-        $user = User::find($event->userId);
-        $user->tokens()->limit(PHP_INT_MAX)->offset(1)->get()->map(function ($token) {
-            $token->revoke();
-        });
+        /*$user = User::find($event->userId);
+        $user->tokens()->limit(PHP_INT_MAX)->offset(1)->get()->map(function ($token) use ($user) {
+           if (!$token->revoked){
+               throw new TwoSessionNotAllowed("Désolé, vous êtes déjà connecté sur un autre appareil");
+           }
+        });*/
     }
 }
