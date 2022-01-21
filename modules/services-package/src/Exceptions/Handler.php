a56ee5528e93097313418f585d5127814cdedfb6
Arafath ATTA YAYA <arafath@dmdconsult.com>
2022-01-21 10:41:49 +0100

    fixing

--- modules/services-package/src/Exceptions/Handler.php
+++ modules/services-package/src/Exceptions/Handler.php
@@ -149,4 +149,13 @@ class Handler
     {
         return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
     }
+
+    public function report(Exception $exception)
+    {
+        // Kill reporting if this is an "access denied" (code 9) OAuthServerException.
+        if ($exception instanceof \League\OAuth2\Server\Exception\OAuthServerException && $exception->getCode() == 9) {
+            return;
+        }
+
+    }
 }
