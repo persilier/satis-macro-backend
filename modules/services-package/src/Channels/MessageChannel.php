ece7f5c39d656af372a8459ba284e0ce558bb4ef
Arafath ATTA YAYA <arafath@dmdconsult.com>
2022-02-01 11:10:06 +0100

    fix notification proof not storing for sms chanel

--- modules/services-package/src/Channels/MessageChannel.php
+++ modules/services-package/src/Channels/MessageChannel.php
@@ -3,8 +3,11 @@
 namespace Satis2020\ServicePackage\Channels;
 
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Satis2020\ServicePackage\MessageApiMethod;
+use Satis2020\ServicePackage\Notifications\AcknowledgmentOfReceipt;
+use Satis2020\ServicePackage\Notifications\CommunicateTheSolution;
 use Satis2020\ServicePackage\Services\SendSMService;
 
 class MessageChannel
@@ -23,7 +26,10 @@ class MessageChannel
     {
         $data = $notification->toMessage($notifiable);
 
-        return (new SendSMService())->send($data);
+        $prove = get_class($notification) == AcknowledgmentOfReceipt::class ||
+            get_class($notification) == CommunicateTheSolution::class;
+
+        return (new SendSMService())->send($data,$prove);
     }
 
 }
