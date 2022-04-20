0882b6ddd7b44d2fb472bea0d310eaf719601c8d
Arafath ATTA YAYA <arafath@dmdconsult.com>
2022-04-20 10:48:59 +0100

    removed reports

--- modules/benchmarking-report/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('/my')->name('my.')->group(function () {
-
-    Route::post('benchmarking-rapport', 'BenchmarkingReportController@index')->name('benchmarking-rapport');
-
-});
