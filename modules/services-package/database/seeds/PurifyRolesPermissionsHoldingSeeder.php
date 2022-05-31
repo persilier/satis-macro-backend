9ad9588db680977b5c1447914351207350c84b81
myriamedmd <myriame@dmdconsult.com>
2022-05-31 11:20:20 +0100

    update report permission and update MyStaffMonitoring

--- modules/services-package/database/seeds/PurifyRolesPermissionsHoldingSeeder.php
+++ modules/services-package/database/seeds/PurifyRolesPermissionsHoldingSeeder.php
@@ -85,7 +85,6 @@ class PurifyRolesPermissionsHoldingSeeder extends Seeder
                     'list-any-notification-proof',
                     'list-regulatory-reporting-claim-my-institution',
                     'config-reporting-claim-any-institution',
-                    'list-system-usage-reporting', 'list-benchmarking-reporting', 'list-global-reporting',
                     'system-any-efficiency-report',
                     'list-reporting-titles-configs','update-reporting-titles-configs','edit-reporting-titles-configs',
                     'show-proxy-config',
