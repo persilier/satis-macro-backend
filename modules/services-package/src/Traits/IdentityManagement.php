08aab401f8c3665aae7fc1d5e0435d671de9928b
Arafath ATTA YAYA <arafath@dmdconsult.com>
2022-06-23 11:39:47 +0100

    fix staff update

--- modules/services-package/src/Traits/IdentityManagement.php
+++ modules/services-package/src/Traits/IdentityManagement.php
@@ -7,6 +7,7 @@ use Illuminate\Validation\Rule;
 use Satis2020\ServicePackage\Models\Identite;
 use Satis2020\ServicePackage\Rules\EmailValidationRules;
 use Satis2020\ServicePackage\Rules\TelephoneArray;
+use Satis2020\ServicePackage\Services\UserService;
 
 trait IdentityManagement
 {
@@ -17,8 +18,15 @@ trait IdentityManagement
 
     protected function updateIdentity($request, $identite)
     {
+        $userService = app(UserService::class);
+
+        if ($request->has('email')){
+            $userService->updateUserByIdentity($identite->id,["username"=>$request->email[0]]);
+        }
+
         $identite->update($request->only(['firstname', 'lastname', 'sexe', 'telephone', 'email', 'ville', 'other_attributes']));
     }
+
 
     /**
      * @return array
