b1a1118014d226f784ec627c019914f5b5420f96
Arafath ATTA YAYA <arafath@dmdconsult.com>
2022-08-19 12:09:44 +0100

    done

--- modules/services-package/database/seeds/PurifyRolesPermissionsFilialSeeder.php
+++ modules/services-package/database/seeds/PurifyRolesPermissionsFilialSeeder.php
@@ -60,6 +60,7 @@ class PurifyRolesPermissionsFilialSeeder extends Seeder
                     'config-reporting-claim-my-institution',
                     'list-notification-proof',
                     'list-reporting-titles-configs','update-reporting-titles-configs','edit-reporting-titles-configs',
+                    'logout-user-my-institution',
                 ],
                 "pilot-filial" => [
                     'list-claim-awaiting-assignment', 'show-claim-awaiting-assignment', 'merge-claim-awaiting-assignment',
