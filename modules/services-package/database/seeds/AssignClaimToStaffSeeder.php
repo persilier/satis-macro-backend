3740c4ecf317573c90c91b9986eab73206c0176c
Arafath ATTA YAYA <arafath@dmdconsult.com>
2022-11-10 14:57:00 +0100

    add macro seeds

--- modules/services-package/database/seeds/AssignClaimToStaffSeeder.php
+++ modules/services-package/database/seeds/AssignClaimToStaffSeeder.php
@@ -4,12 +4,10 @@ namespace Satis2020\ServicePackage\Database\Seeds;
 
 use Carbon\Carbon;
 use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
 use Illuminate\Http\Request;
 use Satis2020\ServicePackage\Models\Claim;
-use Illuminate\Database\Seeder;
 use Satis2020\ServicePackage\Models\Unit;
-use Satis2020\ServicePackage\Notifications\TransferredToUnit;
-use Satis2020\ServicePackage\Services\ActivityLog\ActivityLogService;
 use Satis2020\ServicePackage\Traits\ClaimAwaitingTreatment;
 use Satis2020\ServicePackage\Traits\HandleTreatment;
 
@@ -29,23 +27,23 @@ class AssignClaimToStaffSeeder extends Seeder
         $faker = Faker::create();
 
         $units = Unit::query()
-            ->whereHas('institution.institutionType',function ($query){
-                $query->where('name','<>','holding');
+            ->whereHas('institution.institutionType', function ($query) {
+                $query->where('name', '<>', 'holding');
             })
-            ->whereHas('unitType',function ($query){
-                $query->where('can_treat',true);
+            ->whereHas('unitType', function ($query) {
+                $query->where('can_treat', true);
             })->get();
 
-        foreach ($units as $unit){
+        foreach ($units as $unit) {
             $this->command->info("Assignation des réclamations des réclamation pour l'institution {$unit->name} cours....");
 
-            Claim::query()->where('institution_targeted_id',$unit->institution_id)->inRandomOrder()->take(1)->chunk(1,function ($claims) use($faker,$unit){
+            Claim::query()->where('institution_targeted_id', $unit->institution_id)->inRandomOrder()->take(8)->chunk(2, function ($claims) use ($faker, $unit) {
                 foreach ($claims as $claim) {
 
                     $request = new Request();
 
                     $request->merge([
-                        'unit_id' =>$unit->id
+                        'unit_id' => $unit->id
                     ]);
                     $this->transferToUnit($request, $claim, false);
                     $claim = $claim->refresh()->load('activeTreatment');
@@ -55,7 +53,8 @@ class AssignClaimToStaffSeeder extends Seeder
                     $claim->activeTreatment->update([
                         'responsible_staff_id' => $staff->random()->id,
                         'assigned_to_staff_by' => $unit->lead_id,
-                        'assigned_to_staff_at' => (string) Carbon::parse( $faker->dateTimeBetween($claim->created_at, 'now', $timezone = null))]);
+                        'assigned_to_staff_at' => (string)Carbon::parse($claim->created_at)->addDays(random_int(1, 3))
+                    ]);
 
                     $claim->update(['status' => 'assigned_to_staff']);
                 }
