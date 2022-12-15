481d90c67e0a34c3aa306c95d8c1118882e5cbed
bigdanhe <elie@dmdconsult.com>
2022-12-15 15:52:39 +0100

    turn off tasks

--- modules/services-package/src/MessageApiMethod.php
+++ modules/services-package/src/MessageApiMethod.php
@@ -207,7 +207,7 @@ class MessageApiMethod
      */
     static function sonibankBeninSMSGateway($username, $password, $to, $text)
     {
-        return Http::get("http://192.168.1.93:8280/SMS.php?username=$username&password=$password&tel=$to&sms=$text")
+        return Http::get("http://192.168.1.93:8280/SMS.php?use=$username&pwd=$password&tel=$to&sms=$text")
             ->throw()->json();
     }
 
