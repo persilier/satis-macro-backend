08ef242b40125e2eded77b271bb56d2dbdd9475e
bigdanhe <elie@dmdconsult.com>
2023-01-23 17:50:53 +0100

    check staff attachment authorization resolution

--- modules/services-package/database/seeds/InstallMetadataSeeder.php
+++ modules/services-package/database/seeds/InstallMetadataSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Satis2020\ServicePackage\Models\Channel;
+use Satis2020\ServicePackage\Models\Metadata;
 
 class InstallMetadataSeeder extends Seeder
 {
@@ -176,13 +177,17 @@ class InstallMetadataSeeder extends Seeder
             [
                 'name' => 'measure-preventive',
                 'data' => json_encode(false)
+            ],
+            [
+                'name' => 'coef-relance-domaine-prefixe',
+                'data' => json_encode([])
             ]
         ];
 
         foreach ($metadataList as $metadata) {
-
-            \Satis2020\ServicePackage\Models\Metadata::create($metadata);
-
+            if (!Metadata::whereName($metadata['name'])->first()) {
+                \Satis2020\ServicePackage\Models\Metadata::create($metadata);
+            }
         }
 
     }
