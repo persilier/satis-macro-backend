ad2483e0a6e42cc56d5f5d402058f1ba3191e007
Patrickdmd <amoussou@dmdconsult.com>
2023-01-23 15:57:15 +0100

    configuration relqnce

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -162,7 +162,7 @@ class DatabaseSeeder extends Seeder
         $this->call(\Satis2020\ServicePackage\Database\Seeds\ReportingTitlesSeeder::class);
         $this->call(\Satis2020\ServicePackage\Database\Seeds\DelaiQualificationTreatmentSeeder::class);
         $this->call(\Satis2020\ServicePackage\Database\Seeds\ReportingTitlesSeeder::class);
-       // $this->call(\Satis2020\ServicePackage\Database\Seeds\MetadataTableSeeder::class);
+        $this->call(\Satis2020\ServicePackage\Database\Seeds\MetadataTableSeeder::class);
         //$this->call(RevokeTokensSeeder::class);
 
 
