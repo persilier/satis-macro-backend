0c2608ec67b4fdcd031e03bf97416571c15095ce
Harold FAFOLHAN <harold@dmdconsult.com>
2023-01-23 11:29:35 +0100

    fix bis

--- modules/services-package/src/Traits/ClaimAwaitingTreatment.php
+++ modules/services-package/src/Traits/ClaimAwaitingTreatment.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Satis2020\ServicePackage\Traits;
-
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
@@ -17,14 +15,12 @@ use Satis2020\ServicePackage\Models\Institution;
 use Satis2020\ServicePackage\Models\Staff;
 use Satis2020\ServicePackage\Models\Unit;
 use Satis2020\ServicePackage\Notifications\RejectAClaim;
-
 /**
- * Trait ClaimAwaitingTreatment
- * @package Satis2020\ServicePackage\Traits
- */
+* Trait ClaimAwaitingTreatment
+* @package Satis2020\ServicePackage\Traits
+*/
 trait ClaimAwaitingTreatment
 {
-
     /**
      * @param $institutionId
      * @param $unitId
@@ -32,7 +28,7 @@ trait ClaimAwaitingTreatment
      */
     protected function getClaimsQuery($institutionId, $unitId)
     {
-        return DB::table('claims')
+         $claims = Claim::query()
             ->select('claims.*')
             ->leftJoin('staff', function ($join) {
                 $join->on('claims.created_by', '=', 'staff.id');
@@ -48,9 +44,10 @@ trait ClaimAwaitingTreatment
                 '(`treatments`.`transferred_to_unit_at` IS NOT NULL) and (`treatments`.`responsible_unit_id` = ?)',
                 [$unitId]
             )
-            ->whereNull('claims.deleted_at');
+            ->whereNull('claims.deleted_at')
+            ->with($this->getRelationsAwitingTreatment());
+         return $claims;
     }
-
     /**
      * @param $unitId
      * @param $claim
@@ -60,37 +57,26 @@ trait ClaimAwaitingTreatment
     protected function getOneClaimQuery($unitId, $claim)
     {
         $claim = Claim::with($this->getRelationsAwitingTreatment())->findOrFail($claim);
-
         try {
-
             if ($claim->activeTreatment->responsible_unit_id != $unitId || $claim->status != "transferred_to_unit") {
-
                 throw new CustomException("Impossible de traiter cette réclammation");
             }
-
         } catch (\Exception $exception) {
-
             throw new CustomException("Imposible de récupérer cette réclammation.");
-
         }
-
         return $claim;
     }
-
     /**
      * @param $staff
      * @return bool
      */
     protected function checkLead($staff)
     {
-
         if (Unit::where('lead_id', $staff->id)->find($staff->unit_id)) {
             return true;
         }
-
         return false;
     }
-
     /**
      * @param $claim
      * @param $staffId
@@ -99,12 +85,9 @@ trait ClaimAwaitingTreatment
     protected function assignmentClaim($claim, $staffId)
     {
         $claim->activeTreatment->update(['responsible_staff_id' => $staffId, 'assigned_to_staff_by' => $this->staff()->id, 'assigned_to_staff_at' => Carbon::now()]);
-
         $claim->update(['status' => 'assigned_to_staff']);
-
         return $claim;
     }
-
     /**
      * @param $claim
      * @param $request
@@ -118,7 +101,6 @@ trait ClaimAwaitingTreatment
             'rejected_at' => Carbon::now(),
             'number_reject' => (int) $claim->activeTreatment->number_reject + 1,
         ]);
-
         if (!is_null($claim->transfered_to_targeted_institution_at)) {
             $claim->update(['status' => 'transferred_to_targeted_institution']);
             $institution = Institution::find($claim->institution_targeted_id);
@@ -126,19 +108,15 @@ trait ClaimAwaitingTreatment
             $claim->update(['status' => 'full']);
             $institution = is_null($claim->createdBy) ? $claim->institutionTargeted : $claim->createdBy->institution;
         }
-
         if (!is_null($this->getInstitutionPilot($institution))) {
             $this->getInstitutionPilot($institution)->notify(new RejectAClaim($claim));
         }
-
         try {
             \Illuminate\Support\Facades\Notification::send($this->getUnitStaffIdentities($claim->activeTreatment->responsible_unit_id), new RejectAClaim($claim));
         } catch (\Exception $exception) {
         }
-
         return $claim;
     }
-
     /**
      * @return array
      */
@@ -163,7 +141,6 @@ trait ClaimAwaitingTreatment
             'activeTreatment.responsibleUnit'
         ];
     }
-
     /**
      * @param $staff
      * @param string $assignment
@@ -171,32 +148,25 @@ trait ClaimAwaitingTreatment
      */
     protected function rules($staff, $assignment = 'assignment')
     {
-
         if ($assignment === 'assignment') {
             $data['staff_id'] = ['required', Rule::exists('staff', 'id')->where(function ($query) use ($staff) {
                 $query->where('unit_id', $staff->unit_id);
             })];
         }
-
         if ($assignment === 'unfounded') {
             $data['unfounded_reason'] = ['required', 'string'];
         }
-
         if ($assignment === 'rejected') {
             $data['rejected_reason'] = ['required', 'string'];
         }
-
         if ($assignment === 'treatment') {
             $data['amount_returned'] = ['nullable','filled', 'integer'];
             $data['solution'] = ['required', 'string'];
             $data['comments'] = ['required', 'string'];
             $data['preventive_measures'] = ['string'];
         }
-
         return $data;
     }
-
-
     /**
      * @param $institutionId
      * @param $unitId
@@ -223,7 +193,6 @@ trait ClaimAwaitingTreatment
             )
             ->whereNull('claims.deleted_at');
     }
-
     /**
      * @param $institutionId
      * @param $unitId
@@ -234,73 +203,51 @@ trait ClaimAwaitingTreatment
      */
     protected function getOneClaimQueryTreat($institutionId, $unitId, $staffId, $claim)
     {
-
         if (!$claim = $this->getClaimsTreat($institutionId, $unitId, $staffId)->where('claims.id', $claim)->first())
             throw new CustomException("Impossible de récupérer cette réclammation");
         else
             return Claim::with($this->getRelationsAwitingTreatment())->find($claim->id);
     }
-
-    protected function getTargetedStaffFromUnit($unitId,$isReassignment=false,$currentStaffId=null)
+    protected function getTargetedStaffFromUnit($unitId)
     {
         return Staff::with('identite.user')
             ->where('unit_id', $unitId)
             ->get()
-            ->filter(function ($value, $key) use($isReassignment,$currentStaffId){
+            ->filter(function ($value, $key) {
                 if (is_null($value->identite)) {
                     return false;
                 }
                 if (is_null($value->identite->user)) {
                     return false;
                 }
-                if ($value->identite!=null && $value->identite->user!=null && $value->identite->user->disabled_at!=null) {
-                    return false;
-                }
-
-                if ($isReassignment && $currentStaffId==$value->id){
-                    return false;
-                }
                 return $value->identite->user->hasRole('staff');
             })
             ->values();
     }
-
     protected function canRejectClaim($claim)
     {
         $claim->load(['activeTreatment']);
-
         try {
             $settings = json_decode(Metadata::ofName('reject-unit-transfer-limitation')->firstOrFail()->data);
             $numberReject = (int)$claim->activeTreatment->number_reject;
         } catch (\Exception $exception) {
             return false;
         }
-
         $numberRejectMax = (int)$settings->number_reject_max;
-
         return $numberReject < $numberRejectMax;
     }
-
     /**
      * @return mixed
      */
     protected function queryClaimReassignment(){
-
         return Claim::with($this->getRelationsAwitingTreatment())->whereHas('activeTreatment', function ($query){
-
             $query->where('responsible_staff_id', '!=' ,NULL)->where('responsible_unit_id', $this->staff()->unit_id);
-
         })->whereStatus('assigned_to_staff');
     }
-
-
     protected function checkLeadReassignment(){
-
         $staff = $this->staff();
-
         if (!$this->checkLead($staff)) {
             throw new CustomException("Seul le lead de votre unité est autorisé à effectuer cette action.");
         }
     }
-
 }
