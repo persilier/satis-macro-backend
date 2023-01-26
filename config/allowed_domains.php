fc3bae7cc2834fbe3c58b1d3c5c11e60ccd5fece
Patrickdmd <amoussou@dmdconsult.com>
2023-01-26 16:52:11 +0100

    add url configuration

--- config/allowed_domains.php
+++ config/allowed_domains.php
@@ -4,4 +4,6 @@ return [
    "email_service_url"=>env("MAIL_SERVICE_URL"),
    "mobile_backoffice_url"=>env("MOBILE_BACKOFFICE_URL"),
    "wordpress_plugin_url"=>env("WORDPRESS_PLUGIN_URL"),
+   "bci_online_url"=>env("BCI_ONLINE_URL"),
+   "bci_net_url"=>env("BCI_NET_URL"),
 ];
