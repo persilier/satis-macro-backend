006f7fdd410a2783f817d05dcbbc304444a05179
bigdanhe <elie@dmdconsult.com>
2023-02-02 16:35:40 +0100

    add role description seeder to database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -125,7 +125,7 @@ class DatabaseSeeder extends Seeder
 //        $this->call(\Satis2020\ServicePackage\Database\Seeds\InitializeChannelsTableSeeder::class);
 //        $this->call(\Satis2020\ServicePackage\Database\Seeds\UpdateTimeLimitToClaimTableSeeder::class);
 //        $this->call(\Satis2020\Notification\Database\Seeds\RevokeClaimNotificationsTableSeeder::class);
-//        $this->call(\Satis2020\ServicePackage\Database\Seeds\RoleDescriptionSeed::class);
+          $this->call(\Satis2020\ServicePackage\Database\Seeds\RoleDescriptionSeed::class);
 //         $this->call(\Satis2020\ServicePackage\Database\Seeds\TruncateNotifJobs::class);
 
         /**************** Installation Seed **************************/
