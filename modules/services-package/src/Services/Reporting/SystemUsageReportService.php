4c0aa976219eec633311dc6cf41805357da4c5d4
Patrickdmd <amoussou@dmdconsult.com>
2023-02-20 11:35:39 +0100

    totalTreatedClaimsInTimeLimit, totalTreatedClaimsOutTimeLimit, totalSatisfactionMeasured, complainantSatisfiedInPeriod, complainantSatisfiedOutPeriod

--- modules/services-package/src/Services/Reporting/SystemUsageReportService.php
+++ modules/services-package/src/Services/Reporting/SystemUsageReportService.php
@@ -22,13 +22,32 @@ class SystemUsageReportService
         $totalTreatedClaims = $this->getClaimsTreatedByPeriod($request,Claim::CLAIM_VALIDATED)->count();
         $totalSatisfactionMeasured = $this->getClaimsSatisfactionMeasured($request,Claim::CLAIM_VALIDATED)->count();
 
+        //Nombre de plaintes traitées sur la période et dans les délais
+        $totalTreatedClaimsInTimeLimit = $this->getClaimsTreatedByPeriodInTimeLimit($request,Claim::CLAIM_VALIDATED)->count();
+
+        //Nombre de plaintes traitées sur la période et hors  délais
+        $totalTreatedClaimsOutTimeLimit = $this->getClaimsTreatedByPeriodOutTimeLimit($request,Claim::CLAIM_VALIDATED)->count();
+
+        //nombre de plaignant satisfait dans la période
+        $complainantSatisfiedInPeriod = $this->getComplainantSatisfiedInPeriod($request,"satisfied")->count();
+
+        //nombre de plaignant non satisfait dans la période
+        $complainantSatisfiedOutPeriod = $this->getComplainantSatisfiedInPeriod($request,"unsatisfied")->count();
+
+        //nombre de plaintes par catégorie de réclamations dans la période
+      //  $claimsByCategoryByPeriod = $this->getAllClaimsByCategoryByPeriod($request)->count();
+
+
         return [
             'title' => $this->getMetadataByName(Constants::SYSTEM_USAGE_REPORTING)->title,
             'description' => $this->getMetadataByName(Constants::SYSTEM_USAGE_REPORTING)->description,
-
             'totalReceivedClaims'=>$totalReceivedClaims,
             'totalTreatedClaims'=>$totalTreatedClaims,
-            'totalSatisfactionMeasured'=>$totalSatisfactionMeasured
+            'totalTreatedClaimsInTimeLimit'=>$totalTreatedClaimsInTimeLimit,
+            'totalTreatedClaimsOutTimeLimit'=>$totalTreatedClaimsOutTimeLimit,
+            'totalSatisfactionMeasured'=>$totalSatisfactionMeasured,
+            'complainantSatisfiedInPeriod'=>$complainantSatisfiedInPeriod,
+            'complainantSatisfiedOutPeriod'=>$complainantSatisfiedOutPeriod,
         ];
     }
 
