78190995aa1aa8b538958ddb3277b7bb543ee251
bigdanhe <elie@dmdconsult.com>
2023-02-21 14:52:44 +0100

    satisfaction mesured

--- modules/services-package/src/Traits/ClaimSatisfactionMeasured.php
+++ modules/services-package/src/Traits/ClaimSatisfactionMeasured.php
@@ -31,7 +31,7 @@ trait ClaimSatisfactionMeasured
                 ->on('claims.active_treatment_id', '=', 'treatments.id')->where('treatments.responsible_staff_id', '!=', NULL);
         })->where(function ($query)  use ($statusColumn, $status) {
             $query->where("claims.$statusColumn", $status)
-                ->when($status == Claim::CLAIM_ARCHIVED, function ($queryEscalation) use ($status){
+                ->when($status == Claim::CLAIM_ARCHIVED, function ($queryEscalation) use ($status) {
                     $queryEscalation->orWhere("claims.escalation_status", $status);
                 });
         })->select('claims.*');
@@ -98,7 +98,7 @@ trait ClaimSatisfactionMeasured
                 return ($this->institution() && $item->activeTreatment->responsibleStaff && $this->institution()->id === $item->activeTreatment->responsibleStaff->institution_id);
             })->values();
     }
- /**
+    /**
      * @param string $status
      * @param bool $paginate
      * @param int $paginationSize
@@ -107,35 +107,33 @@ trait ClaimSatisfactionMeasured
      * @return mixed
      */
 
-     protected function getAllForSatisfactionMyClaim($status = 'validated', $paginate = false, $paginationSize = 10, $key = null, $statusColumn = 'status', $satisfactionStaffConcerned = false)
-     {
- 
-         return $paginate
-             ? $this->getClaim($status, $statusColumn)
-             ->where('institution_targeted_id', $this->institution()->id)
-             ->when($satisfactionStaffConcerned === true, function ($queryStaff1) {
-                 $queryStaff1->whereHas('activeTreatment', function ($queryStaff2) {
-                     $queryStaff2->whereNotNull('satisfaction_responsible_staff_id', $this->staff()->id)
-                         ->whereNotNull('transfered_to_satisfaction_responsible_at');
-                 });
-             })
-             ->when($key, function (Builder $query1) use ($key) {
-                 $query1->where('reference', 'LIKE', "%$key%")
-                     ->orWhereHas("claimer", function ($query2) use ($key) {
-                         $query2->where('firstname', 'LIKE', "%$key%")
-                             ->orWhere('lastname', 'LIKE', "%$key%")
-                             ->orwhereJsonContains('telephone', $key)
-                             ->orwhereJsonContains('email', $key);
-                     })->orWhereHas("claimObject", function ($query3) use ($key) {
-                         $query3->where("name->" . App::getLocale(), 'LIKE', "%$key%");
-                     })->orWhereHas("unitTargeted", function ($query4) use ($key) {
-                         $query4->where("name->" . App::getLocale(), 'LIKE', "%$key%");
-                     });
-             })->paginate($paginationSize)
-             : $this->getClaim($status)->get()->filter(function ($item) {
-                 return ($this->institution() && $item->activeTreatment->responsibleStaff && $this->institution()->id === $item->activeTreatment->responsibleStaff->institution_id);
-             })->values();
-     }
+    protected function getAllForSatisfactionMyClaim($status = 'validated', $paginate = false, $paginationSize = 10, $key = null, $statusColumn = 'status')
+    {
+
+        return $paginate
+            ? $this->getClaim($status, $statusColumn)
+            ->where('institution_targeted_id', $this->institution()->id)
+            ->whereHas('activeTreatment', function ($queryStaff2) {
+                $queryStaff2->where('satisfaction_responsible_staff_id', $this->staff()->id)
+                    ->whereNotNull('transfered_to_satisfaction_responsible_at');
+            })
+            ->when($key, function (Builder $query1) use ($key) {
+                $query1->where('reference', 'LIKE', "%$key%")
+                    ->orWhereHas("claimer", function ($query2) use ($key) {
+                        $query2->where('firstname', 'LIKE', "%$key%")
+                            ->orWhere('lastname', 'LIKE', "%$key%")
+                            ->orwhereJsonContains('telephone', $key)
+                            ->orwhereJsonContains('email', $key);
+                    })->orWhereHas("claimObject", function ($query3) use ($key) {
+                        $query3->where("name->" . App::getLocale(), 'LIKE', "%$key%");
+                    })->orWhereHas("unitTargeted", function ($query4) use ($key) {
+                        $query4->where("name->" . App::getLocale(), 'LIKE', "%$key%");
+                    });
+            })->paginate($paginationSize)
+            : $this->getClaim($status)->get()->filter(function ($item) {
+                return ($this->institution() && $item->activeTreatment->responsibleStaff && $this->institution()->id === $item->activeTreatment->responsibleStaff->institution_id);
+            })->values();
+    }
 
     /**
      * @param int $paginationSize
