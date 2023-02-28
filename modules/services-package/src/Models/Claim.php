249e7670dd455b2c4779b507427a3f648356fc88
Sassus Capelo <synedcap@gmail.com>
2023-02-28 15:23:40 +0100

    correction update claim object

--- modules/services-package/src/Models/Claim.php
+++ modules/services-package/src/Models/Claim.php
@@ -160,12 +160,15 @@ class Claim extends Model
         if ($this->time_limit && $this->created_at && ($this->status !== 'archived')) {
 
             $claimInfo = Treatment::where('claim_id',$this->id)->first();
-            if ($claimInfo && $claimInfo->solved_at !== null) {
+            $duration_done = $this->daysWithoutWeekEnd($claimInfo->assigned_to_staff_at,$claimInfo->solved_at);
+            $ecart = $this->conversion($this->time_treatment) -  $duration_done;
+
+           /*  if ($claimInfo && $claimInfo->solved_at !== null) {
                
                 $duration_done = $this->daysWithoutWeekEnd($claimInfo->assigned_to_staff_at,$claimInfo->solved_at);
                 $ecart = $this->conversion($this->time_treatment) -  $duration_done;
   
-            }
+            } */
         }
 
         return [
