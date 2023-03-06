07a33c8936f030ed70cbbb532c351fcd362d48b8
bigdanhe <elie@dmdconsult.com>
2023-03-06 15:00:37 +0100

    add permissions to admin

--- modules/services-package/database/seeds/PurifyRolesPermissionsIndependantSeeder.php
+++ modules/services-package/database/seeds/PurifyRolesPermissionsIndependantSeeder.php
@@ -92,7 +92,7 @@ class PurifyRolesPermissionsIndependantSeeder extends Seeder
                     'bci-monthly-reports', 'bci-annual-reports',
                     'export-notification-proof', 'configuration-pilot',
                     'configure-pilot-collector-discussion-attribute',
-                    'activity-log'
+                    'activity-log' , 'list-auth-config','update-auth-config'
                 ],
                 "pilot" => [
                     'list-claim-awaiting-assignment', 'show-claim-awaiting-assignment', 'merge-claim-awaiting-assignment',
