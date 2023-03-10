39b8437b096dda2d81e2fd23deeedf2b0a270336
Sassus Capelo <synedcap@gmail.com>
2023-03-10 12:08:13 +0100

    correction time transfert to unit

--- modules/services-package/src/Models/Claim.php
+++ modules/services-package/src/Models/Claim.php
@@ -172,7 +172,7 @@ class Claim extends Model
         return [
             "global_delay" => $this->time_limit,
             "Quota_delay_assigned" => $this->time_unit,
-            "duration_done" => $duration_done['minutes']. ' min, ',
+            "duration_done" => $duration_done['days'],
             "ecart" =>  $ecart,
            
         ];
@@ -196,7 +196,7 @@ class Claim extends Model
         return [
             "global_delay" => $this->time_limit,
             "Quota_delay_assigned" => $this->time_staff,
-            "duration_done" => $duration_done['minutes']. ' min, ',
+            "duration_done" => $duration_done['days'],
             "ecart" =>  $ecart,
            
         ];
@@ -220,7 +220,7 @@ class Claim extends Model
         return [
             "global_delay" => $this->time_limit,
             "Quota_delay_assigned" => $this->time_treatment,
-            "duration_done" => $duration_done['minutes']. ' min, ',
+            "duration_done" => $duration_done['days'],
             "ecart" =>  $ecart,
            
         ];
@@ -244,7 +244,7 @@ class Claim extends Model
         return [
             "global_delay" => $this->time_limit,
             "Quota_delay_assigned" => $this->time_validation,
-            "duration_done" => $duration_done['minutes']. ' min, ',
+            "duration_done" => $duration_done['days'],
             "ecart" =>  $ecart,
            
         ];
@@ -269,7 +269,7 @@ class Claim extends Model
         return [
             "global_delay" => $this->time_limit,
             "Quota_delay_assigned" => $this->time_measure_satisfaction,
-            "duration_done" => $duration_done['minutes']. ' min, ',
+            "duration_done" => $duration_done['days'],
             "ecart" =>  $ecart,
            
         ];
