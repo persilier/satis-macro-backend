00ebc4ec040535b9c48d90b6bc07006918bbb536
bigdanhe <elie@dmdconsult.com>
2023-03-24 11:37:04 +0100

    add quota delay permission to admin-holding

--- modules/services-package/database/seeds/PurifyRolesPermissionsHoldingSeeder.php
+++ modules/services-package/database/seeds/PurifyRolesPermissionsHoldingSeeder.php
@@ -87,6 +87,8 @@ class PurifyRolesPermissionsHoldingSeeder extends Seeder
                     'show-proxy-config', 'update-proxy-config', 'delete-proxy-config',
                     'list-config-reporting-claim-my-institution', 'store-config-reporting-claim-my-institution', 'update-config-reporting-claim-my-institution', 'delete-config-reporting-claim-my-institution',
                     'list-reporting-titles-configs', 'update-reporting-titles-configs', 'edit-reporting-titles-configs',
+                    'show-configuration-quota-delay',
+                    'update-configuration-quota-delay',
                 ],
                 "pilot-holding" => [
                     'list-claim-awaiting-assignment', 'show-claim-awaiting-assignment', 'merge-claim-awaiting-assignment',
